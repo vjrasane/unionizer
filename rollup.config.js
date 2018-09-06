c271d4bf4d105920971372e9072f4beee9f69490
Ville Räsänen <ville.rasanen@aalto.fi>
2018-09-07 01:30:52 +0300

    Cleaning up this and that

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,28 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import filesize from 'rollup-plugin-filesize';
+import progress from 'rollup-plugin-progress';
 import { uglify } from 'rollup-plugin-uglify';
 
-// `npm run build` -> `production` is true
-// `npm run dev` -> `production` is false
-const production = !process.env.ROLLUP_WATCH;
-
 export default {
-  external: ['path', 'fs'],
+  external: ['path', 'fs', 'deepmerge'],
   input: 'src/main.js',
   output: {
     file: 'dist/main.js',
-    format: 'cjs', // immediately-invoked function expression — suitable for <script> tags
+    format: 'cjs',
     sourcemap: true
   },
   plugins: [
-    resolve(
-      // preferBuiltins: false
-    ),
-    production &&
-      babel({
-        babelrc: false,
-        presets: [['env', { modules: false }]],
-        plugins: [
-          'external-helpers',
-          'transform-class-properties',
-          'transform-object-rest-spread'
-        ]
-      }),
-    commonjs(),
-    production && uglify()
+    progress(),
+    babel({
+      babelrc: false,
+      presets: [['env', { modules: false }]],
+      plugins: [
+        'external-helpers',
+        'transform-class-properties',
+        'transform-object-rest-spread'
+      ]
+    }),
+    uglify(),
+    filesize()
   ]
 };
